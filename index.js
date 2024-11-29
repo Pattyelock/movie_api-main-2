ade0fb758a1cd2873c5df84a749edafb9045cf98
pattylock <pattycakesinfo@gmail.com>
2024-11-29 14:39:52 +0100

    Update MongoDB connection to use environment variable

--- index.js
+++ index.js
@@ -24,11 +24,7 @@ require("./passport"); // Assuming passport config is in a separate file
 auth(app);
 
 // Connect to MongoDB
-mongoose
-  .connect("mongodb://localhost:27017/myFlixDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB connection error:", error));
 
