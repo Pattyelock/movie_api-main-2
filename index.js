8ed2d8f920abc317a7f11f1d299528603c998b04
pattylock <pattycakesinfo@gmail.com>
2024-11-30 18:35:01 +0100

    Updated files

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the movie API!");
 });
 
+require("dotenv").config();
+
 // Passport config
 require("./passport"); // Assuming passport config is in a separate file
 
@@ -24,7 +26,11 @@ require("./passport"); // Assuming passport config is in a separate file
 auth(app);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log("MongoDB connection error:", error));
 
