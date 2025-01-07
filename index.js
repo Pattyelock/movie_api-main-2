8afba2af270427399df5a6c947f9d49774eae75c
Lucien Chemaly <chemaly.lucien@gmail.com>
2025-01-07 17:24:30 +0200

    adding dynamic port for heroku

--- index.js
+++ index.js
@@ -163,12 +163,7 @@ app.delete(
 );
 
 // Start the server
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
-
-// const port = process.env.PORT || 8080;
-// app.listen(port, "0.0.0.0", () => {
-//   console.log("Listening on Port " + port);
-// });
